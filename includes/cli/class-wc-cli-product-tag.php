028e558122ace00944c178338a3325ec635a1294
Bryan Littlefield <bryan@coolblueweb.com>
2016-05-29 14:28:27 -0700

    [+] Created WC_CLI_Product_Tag class

---
+++ includes/cli/class-wc-cli-product-tag.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Manage Product Categories.
+ *
+ * @since    2.8.0
+ * @package  WooCommerce/CLI
+ * @category CLI
+ * @author   WooThemes
+ */
+class WC_CLI_Product_Tag extends WC_CLI_Command {
+
+}
