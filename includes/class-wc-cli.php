893e3ee7ecf2e8aa84b7ae58a54917fa68747498
Timon Davis <timon.a.davis@gmail.com>
2016-06-05 19:14:01 -0700

    Revert "Ehnancement/wc cli product tags  list"

--- includes/class-wc-cli.php
+++ includes/class-wc-cli.php
@@ -18,7 +18,6 @@ WP_CLI::add_command( 'wc customer',         'WC_CLI_Customer' );
 WP_CLI::add_command( 'wc order',            'WC_CLI_Order' );
 WP_CLI::add_command( 'wc product',          'WC_CLI_Product' );
 WP_CLI::add_command( 'wc product category', 'WC_CLI_Product_Category' );
-WP_CLI::add_command( 'wc product tag', 		'WC_CLI_Product_Tag' );
 WP_CLI::add_command( 'wc report',           'WC_CLI_Report' );
 WP_CLI::add_command( 'wc tax',              'WC_CLI_Tax' );
 WP_CLI::add_command( 'wc tool',             'WC_CLI_Tool' );
